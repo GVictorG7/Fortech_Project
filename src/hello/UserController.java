5fd4359af7f850fbe0d49006339dbb8b4b7891f4
Victor <victor.georgescu07@gmail.com>
2018-05-18 18:20:00 +0300

    Project commit
    MVC2

--- src/hello/UserController.java
+++ src/hello/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import hello.service.StationService;
 import hello.service.UserService;
 
 @Controller
